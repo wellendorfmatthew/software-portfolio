e5e9dcea34d067b24df2449e7b123277f62f65de
Matthew Wellendorf <wellendorfmatthew@gmail.com>
2025-04-16 15:11:29 -0400

    Added about me section

--- software-portfolio/src/app/page.tsx
+++ software-portfolio/src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Header from "./components/header/page";
 import Home from "./components/home/page";
+import AboutMe from "./components/about-me/page";
 
 export default function Root() {
   return (
-    <div className="flex flex-col items-center h-screen w-screen bg-primary gap-40">
+    <div className="flex flex-col items-center gap-40">
       <Header />
       <Home />
+      <AboutMe />
     </div>
   );
 }
